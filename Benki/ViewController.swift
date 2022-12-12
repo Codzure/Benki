c3991de11e323ff5863ec4982a647015368a02ba
Leonard Mutugi Muthoni <leonard.mutugi@equitybank.co.ke>
2022-12-12 14:02:29 +0300

    Initial Commit

---
+++ Benki/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Benki
+//
+//  Created by Leonard Mutugi on 12/12/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
